9b3d65677b77d838675e5d7d76e3e3dd24f34221
Mori Shota <k22rs138@st.kyusan-u.ac.jp>
2024-12-23 09:38:37 +0900

    test2

--- db_inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  //$conn = new mysqli("localhost", "root", "", "wp2024");//＜開発時の環境設定＞
-  $conn = new mysqli("localhost","k22rs138", "Ksu/DB!2024", "wdb24k22rs138");//＜運用時の環境設定＞
-  if ($conn->connect_errno) die($conn->connect_error);
-  $conn->set_charset('utf8'); //文字コードをutf8に設定（文字化け対策）
-?>
--- sys_login.php
+++
@@ -1,7 +0,0 @@
-<form action="?do=sys_check" method="post">
-<table>
-<tr><td>ユーザ名：</td><td><input type="text" name="uid"></td></tr>
-<tr><td>パスワード：</td><td><input type="password" name="pass"></td></tr>
-</table>
-<input type="submit" value="送信"><input type="reset" value="取消">
-</form>
